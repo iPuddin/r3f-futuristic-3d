f44ce9dda9b29fa63bfe3b6845725c8661a41c4c
Puttijak Tuntrajin <tu.puttijak@hybiot.io>
2025-11-22 00:32:30 +0700

    [feat]npm bun install

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  // const [UIcolor, setUIcolor] = useState(null)
+
+  //our callback funtion
+  // const getColor = (color) => {
+  //   setUIcolor(color)
+  // }
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Color Picker App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
